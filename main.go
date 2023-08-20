5c7de8a2d36f410739cff510316ee7c7999b8f76
Martin Landart <17906247+martinlandart@users.noreply.github.com>
2023-08-20 15:21:39 -0300

    refactor

--- main.go
+++ main.go
@@ -7,15 +7,18 @@ import (
 )
 
 func main() {
-
 }
 
-func GoWc(writer io.Writer, args ...string) error {
-	file, err := os.ReadFile("test.txt")
-	if err != nil {
-		return err
-	}
-
-	fmt.Fprintf(writer, "%v test.txt\n", len(file))
+func HandleGetByteCountCommand(w io.Writer, filename string) error {
+	bc, _ := ByteCount(filename)
+	fmt.Fprintf(w, "%v %s\n", bc, filename)
 	return nil
+}
+
+func ByteCount(filename string) (uint, error) {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		return 0, err
+	}
+	return uint(len(file)), nil
 }
--- main_test.go
+++ main_test.go
@@ -2,32 +2,54 @@ package main
 
 import (
 	"bytes"
-	"os/exec"
 	"reflect"
 	"testing"
 )
 
+// func TestByteCount(t *testing.T) {
+// 	wc := exec.Command("wc", "-c", "test.txt")
+// 	var wcout bytes.Buffer
+// 	wc.Stdout = &wcout
+// 	err := wc.Run()
+// 	if err != nil {
+// 		t.Fatalf("wc unexpected error %q", err)
+// 	}
+//
+// 	var goWcOut bytes.Buffer
+// 	os.Args = []string{"-c", "test.txt"}
+// 	err = GoWc(&goWcOut)
+// 	if err != nil {
+// 		t.Fatalf("gowc unexpected error %q", err)
+// 	}
+// 	got := goWcOut.Bytes()
+// 	want := wcout.Bytes()
+// 	if !reflect.DeepEqual(got, want) {
+// 		t.Fatalf("got %q want %q", got, want)
+// 	}
+// }
+
+var testFile string = "test.txt"
+
+func TestHandleGetByteCountCommand(t *testing.T) {
+	var got bytes.Buffer
+	err := HandleGetByteCountCommand(&got, testFile)
+	if err != nil {
+		t.Fatalf("unexpected error %q", err)
+	}
+
+	want := bytes.NewBufferString("335191 test.txt\n")
+	if !reflect.DeepEqual(got, *want) {
+		t.Fatalf("got %s want %s", got.Bytes(), want.Bytes())
+	}
+}
+
 func TestByteCount(t *testing.T) {
-	wc := exec.Command("wc", "-c", "test.txt")
-
-	var wcout bytes.Buffer
-	wc.Stdout = &wcout
-
-	err := wc.Run()
+	want := uint(335191)
+	got, err := ByteCount(testFile)
 	if err != nil {
-		t.Errorf("wc unexpected error %q", err)
+		t.Fatalf("unexpected error %q", err)
 	}
-
-	var goWcOut bytes.Buffer
-
-	err = GoWc(&goWcOut, "-c", "test.txt")
-	if err != nil {
-		t.Errorf("gowc unexpected error %q", err)
-	}
-
-	got := goWcOut.Bytes()
-	want := wcout.Bytes()
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %q want %q", got, want)
+	if got != want {
+		t.Fatalf("got %v want %v", got, want)
 	}
 }
